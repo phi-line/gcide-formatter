e07017631d190c89db74b861eebeebe5b37c7253
Noah Jutz <noahjutz@tutanota.de>
2021-07-12 19:44:54 +0200

    Create DB and table

--- gcide_json_to_sqlite3.py
+++ gcide_json_to_sqlite3.py
@@ -2,6 +2,7 @@
 
 import gcide_to_json_converter as gcide_json
 import json
+import sqlite3
 
 # Step 1: Get json
 print("Retrieving json")
@@ -15,3 +16,24 @@ entries = json.loads(json_str)
 entries = map(lambda j: gcide_json.Entry.from_json(j), entries)
 entries = list(entries)
 
+# Step 3: Create an Sqlite3 database
+print("Creating gcide.db")
+
+db = sqlite3.connect("gcide.db")
+
+# Step 4: Create a table
+print("Creating Definitions table")
+
+db.execute(
+    """
+    CREATE TABLE Definitions (
+    id          INT         NOT NULL    PRIMARY KEY,
+    word        TEXT        NOT NULL,
+    text        TEXT        NOT NULL,
+    source      TEXT        NOT NULL,
+    pos         TEXT        NOT NULL
+    );
+    """
+)
+
+db.close()
