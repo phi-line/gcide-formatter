19034677756171b77e70f3ca7718419754335d47
Noah Jutz <noahjutz@tutanota.de>
2021-07-30 14:08:07 +0200

    Wrap gcide_sqlite.py code in function

--- gcide_sqlite.py
+++ gcide_sqlite.py
@@ -3,59 +3,52 @@
 import gcide_parser
 import sqlite3
 
-"""
-Inserts parsed definitions to sqlite database
-"""
 
-# Get definitions
-print("Getting definitions")
+def create_definition_database(definitions=gcide_parser.xml_to_objects()):
+    # Create an Sqlite3 database
+    print("Creating gcide.db")
 
-definitions = gcide_parser.xml_to_objects()
+    db = sqlite3.connect("gcide.db")
 
-# Create an Sqlite3 database
-print("Creating gcide.db")
+    # Create a table
+    print("Creating Definitions table")
 
-db = sqlite3.connect("gcide.db")
-
-# Create a table
-print("Creating Definitions table")
-
-db.execute(
-    """
-    CREATE TABLE Definitions (
-    id          INT         NOT NULL    PRIMARY KEY,
-    word        TEXT        NOT NULL,
-    text        TEXT        NOT NULL,
-    source      TEXT        NOT NULL,
-    pos         TEXT        NOT NULL
-    );
-    """
-)
-
-# Insert definitions to table
-print("Inserting definitions")
-
-for i, definition in enumerate(definitions):
-    definition = gcide_parser.Definition(
-        definition.word.replace("\"", "''"),
-        definition.text.replace("\"", "''"),
-        definition.source.replace("\"", "''"),
-        definition.pos.replace("\"", "''")
-    )
     db.execute(
-        f"""
-        INSERT INTO Definitions (id, word, text, source, pos) VALUES (
-            {i},
-            "{definition.word}",
-            "{definition.text}",
-            "{definition.source}",
-            "{definition.pos}"
-        ) 
+        """
+        CREATE TABLE Definitions (
+        id          INT         NOT NULL    PRIMARY KEY,
+        word        TEXT        NOT NULL,
+        text        TEXT        NOT NULL,
+        source      TEXT        NOT NULL,
+        pos         TEXT        NOT NULL
+        );
         """
     )
 
-# Close DB
-print("Closing database")
+    # Insert definitions to table
+    print("Inserting definitions")
 
-db.commit()
-db.close()
+    for i, definition in enumerate(definitions):
+        definition = gcide_parser.Definition(
+            definition.word.replace("\"", "''"),
+            definition.text.replace("\"", "''"),
+            definition.source.replace("\"", "''"),
+            definition.pos.replace("\"", "''")
+        )
+        db.execute(
+            f"""
+            INSERT INTO Definitions (id, word, text, source, pos) VALUES (
+                {i},
+                "{definition.word}",
+                "{definition.text}",
+                "{definition.source}",
+                "{definition.pos}"
+            ) 
+            """
+        )
+
+    # Close DB
+    print("Closing database")
+
+    db.commit()
+    db.close()
