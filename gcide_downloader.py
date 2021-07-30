3b6f67e3a56ee793663484074910db2517a731a1
Noah Jutz <noahjutz@tutanota.de>
2021-07-30 14:06:24 +0200

    Add gcide_downloader.py

---
+++ gcide_downloader.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import requests
+import zipfile
+import shutil
+import os
+
+
+def download_gcide_xml(url='http://www.ibiblio.org/webster/gcide_xml-0.51.zip'):
+    r = requests.get(url, allow_redirects=True)
+    open('gcide_xml-0.51.zip', 'wb').write(r.content)
+    zipfile.ZipFile('gcide_xml-0.51.zip', 'r').extractall('gcide_xml_unzipped')
+    shutil.move('gcide_xml_unzipped/gcide_xml-0.51/xml_files', '.')
+    shutil.rmtree('gcide_xml_unzipped')
+    os.remove('gcide_xml-0.51.zip')
