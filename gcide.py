85560d5f7fe1420e565d1fad0f1c59ebaace5974
Noah Jutz <noahjutz@tutanota.de>
2021-07-31 12:38:59 +0200

    Add documentation to gcide.py

--- gcide.py
+++ gcide.py
@@ -1,16 +1,53 @@
 import click
 import gcide_parser
-import gcide_sqlite
-import gcide_serializer
+import definitions_sqlite
+import definitions_json
+import gcide_downloader
+
 
 @click.group()
 def cli():
+    """
+    Transforms GCIDE_XML into structured data formats (JSON or SQLite3-database). Gcide-Formatter
+    Parses only definitions and their respective sources and part of speech.
+    """
     pass
+
 
 @cli.command()
 def json():
+    """
+    Creates 'gcide.json' file.
+
+    \b
+    JSON Format example:
+    [
+        ...
+        {
+            "id":12,
+            "word":"prolong",
+            "text":"To put off to a distant time; to postpone",
+            "source":"1913 Webster",
+            "pos":"v. t."
+        }
+        ...
+    ]
+    """
     click.echo("json")
+
 
 @cli.command()
 def sqlite():
+    """
+    Creates 'gcide.db' file.
+
+    \b
+    Definitions table columns:
+    id          INT         NOT NULL    PRIMARY KEY,
+    word        TEXT        NOT NULL,
+    text        TEXT        NOT NULL,
+    source      TEXT        NOT NULL,
+    pos         TEXT        NOT NULL
+
+    """
     click.echo("sqlite")
