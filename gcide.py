5944a21b1c0a0232fd184d4b76d8870561b24b95
Noah Jutz <noahjutz@tutanota.de>
2021-08-16 10:36:13 +0200

    Add --fetch-xml and --gcide-dir options

--- gcide.py
+++ gcide.py
@@ -6,11 +6,23 @@ import gcide_downloader
 
 
 @click.group()
-def cli():
+@click.option('--fetch-xml', default=False, help='Download GCIDE_XML files from ibiblio.org/webster/.')
+@click.option('--gcide-dir', default='xml_files', help='GCIDE_XML directory path.')
+def cli(fetch_xml, gcide_dir):
     """
     Transforms GCIDE_XML into structured data formats (JSON or SQLite3-database). Gcide-Formatter
     parses definitions and their respective sources and part of speech.
+
+    A gcide_xml directory is required. You can automatically download it using the --fetch option
+    or download it yourself and pass the directory containing the xml files using the --gcide-dir
+    option.
+
+    Learn more about GCIDE_XML on ibiblio.org/webster/.
     """
+    if (fetch):
+        gcide_downloader.download_gcide_xml()
+    else:
+        global gcide_dir = gcide_dir
     pass
 
 
