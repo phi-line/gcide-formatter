5896916ba07c1fa0b4d2957286581f952a4b6cd3
Noah Jutz <noahjutz@tutanota.de>
2021-08-16 11:07:11 +0200

    Add progress bar to core.py

--- core.py
+++ core.py
@@ -2,6 +2,7 @@ import re
 import os
 from bs4 import BeautifulSoup
 import lxml
+import click
 
 
 class Definition:
@@ -20,42 +21,43 @@ def xml_to_objects(gcide_dir):
     files = filter(lambda name: re.match("^gcide.\\w.xml$", name), files)
     files = list(files)
 
-    for file in files:
-        xml = open(f"{gcide_dir}/{file}", "r")
-        xml = xml.read()
-        xml = "<root>" + xml + "</root>"
-        parser = BeautifulSoup(markup=xml, features="lxml-xml")
-        p_list = parser.find_all("p")
-        p_groups = []
-        for p in p_list:
-            if p.find("ent") is not None:
-                p_group = [p]
-                p_groups.append(p_group)
-            elif p_groups.__len__() < 1:
-                pass
-            else:
-                p_groups[p_groups.__len__() - 1].append(p)
+    with click.progressbar(files, length=len(files), label="Parsing files") as files:
+        for file in files:
+            xml = open(f"{gcide_dir}/{file}", "r")
+            xml = xml.read()
+            xml = "<root>" + xml + "</root>"
+            parser = BeautifulSoup(markup=xml, features="lxml-xml")
+            p_list = parser.find_all("p")
+            p_groups = []
+            for p in p_list:
+                if p.find("ent") is not None:
+                    p_group = [p]
+                    p_groups.append(p_group)
+                elif p_groups.__len__() < 1:
+                    pass
+                else:
+                    p_groups[p_groups.__len__() - 1].append(p)
 
-        for p_group in p_groups:
-            entry = p_group[0].find("ent")
-            entry = entry.text
-            for p in p_group:
-                definitions = p.find_all("def")
-                definitions = map(lambda d: d.text, definitions)
-                definitions = list(definitions)
+            for p_group in p_groups:
+                entry = p_group[0].find("ent")
+                entry = entry.text
+                for p in p_group:
+                    definitions = p.find_all("def")
+                    definitions = map(lambda d: d.text, definitions)
+                    definitions = list(definitions)
 
-                part_of_speech = p.find("pos", recursive=False)
-                part_of_speech = part_of_speech.text if part_of_speech is not None else ""
+                    part_of_speech = p.find("pos", recursive=False)
+                    part_of_speech = part_of_speech.text if part_of_speech is not None else ""
 
-                sources = p.find_all("source")
-                sources = map(lambda s: s.text, sources)
-                sources = list(sources)
-                sources = " ".join(sources)
+                    sources = p.find_all("source")
+                    sources = map(lambda s: s.text, sources)
+                    sources = list(sources)
+                    sources = " ".join(sources)
 
-                for definition in definitions:
-                    print(f"{definition_objects.__len__()} ({entry})")
-                    definition_objects.append(
-                        Definition(entry, definition, sources, part_of_speech)
-                    )
+                    for definition in definitions:
+                        definition_objects.append(
+                            Definition(entry, definition,
+                                       sources, part_of_speech)
+                        )
 
     return definition_objects
